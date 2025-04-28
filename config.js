8d841d35975a2c361358c781de9fed7c73b12833
unknown <juanjosesierram@gmail.com>
2025-04-28 10:01:38 -0500

    Test config file

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "autodiscover": true,
+  "prHourlyLimit": 0,
+  "dependencyDashboard": true,
+  "dependencyDashboardTitle": "Dependency Dashboard",
+  "npmrc": "@jjsierra0318:registry=https://npm.pkg.github.com/"
+}
